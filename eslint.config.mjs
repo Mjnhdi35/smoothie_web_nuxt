7aaf0c488ae6b04ede023b40c8f03085e0ddbece
smoothiestudio <mjnhdi35@gmail.com>
2026-02-19 02:59:41 +0700

    fix lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,4 +3,14 @@ import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt(
   // Your custom configs here
+  [
+    {
+      ignores: ['eslint.config.*'],
+      rules: {
+        '@stylistic/quotes': ['error', 'single'],
+        '@stylistic/comma-dangle': ['error', 'never'],
+        '@stylistic/semi': ['error', 'never']
+      }
+    }
+  ]
 )
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ["@nuxt/eslint", "@nuxt/ui"],
-
   devtools: {
     enabled: true,
   },
-
+  app: {
+    head: {
+      title: "Smoothie Studio",
+      htmlAttrs: {
+        lang: "en",
+      },
+      meta: [{ name: "description", content: "Smoothie Studio" }],
+      link: [
+        {
+          rel: "icon",
+          type: "image/x-icon",
+          href: "/logo.png",
+        },
+      ],
+    },
+  },
   css: ["~/assets/css/main.css"],
 
   routeRules: {
@@ -20,21 +34,6 @@ export default defineNuxtConfig({
         commaDangle: "never",
         braceStyle: "1tbs",
       },
-    },
-  },
-  app: {
-    head: {
-      title: "Smoothie Studio",
-      htmlAttrs: {
-        lang: "en",
-      },
-      link: [
-        {
-          rel: "icon",
-          type: "image/x-icon",
-          href: "/logo.png",
-        },
-      ],
     },
   },
 });
